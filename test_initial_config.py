087dd6e47793f1f837ff658bc95df61b91f0df1e
Szymon Mieszczak <szymon.mieszczak@gmail.com>
2017-06-26 22:52:08 +0200

    Repair tests.

--- test_initial_config.py
+++ test_initial_config.py
@@ -1,23 +1,27 @@
 from main import InitialConfig, Algorithm
+import numpy as np
 
 def test_create_confg():
-    for i in range(100):
+    for i in range(1000):
         q1 = InitialConfig(20)
         config = q1.create_config()
-        for position in range(len(config) - 1):
-            assert abs(config[position][0] - config[position + 1][0] +
-                       config[position + 1][1] - config[position][1]) \
-                   in (1, len(config) - 1, len(config) + 1)
+        for segment in range(1, len(config)):
+            assert (np.sum(abs(config[segment] - config[segment - 1]))  == 1 or
+                        (np.sum(abs(config[segment] - config[segment - 1])) == q1.segment_count))
+
+
 
 def test_movement():
     for i in range(100):
-        q1 = Algorithm(100)
+        q1 = Algorithm(50)
         config = q1.movement(1000)
-        for position in range(len(config) - 1):
-            assert abs(config[position][0] - config[position + 1][0] +
-                       config[position + 1][1] - config[position][1]) \
-                   in (1, len(config) - 1, len(config) + 1)
-    print(config)
+        for segment in range(1, len(config)):
+            if not (np.sum(abs(config[segment] - config[segment - 1]))  == 1 or
+                        (np.sum(abs(config[segment] - config[segment - 1])) == q1.segment_count-1)):
+                print(config)
+                print(config[segment], config[segment - 1], i)
+                raise ValueError
 
-test_create_confg()
+
+#test_create_confg()
 test_movement()
